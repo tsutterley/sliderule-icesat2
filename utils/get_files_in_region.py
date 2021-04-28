4fde7be53e7fa69c6e7865f51643c31397dc0f72
JP Swinski <jp.swinski@nasa.gov>
2021-04-28 10:23:58 -0400

    enhanced get_files_in_region to support tolerance

--- utils/get_files_in_region.py
+++ utils/get_files_in_region.py
@@ -12,18 +12,26 @@ from sliderule import icesat2
 
 if __name__ == '__main__':
 
-    # Override region of interest
-    regions = icesat2.toregion(sys.argv[1])
+    # Set Filename
+    filename = sys.argv[1]
+
+    # Override Tolerance
+    tolerance = 0.0
+    if len(sys.argv) > 2:
+        tolerance = float(sys.argv[2])
 
     # Override dataset
     dataset='ATL03'
-    if len(sys.argv) > 2:
-        dataset = sys.argv[2]
+    if len(sys.argv) > 3:
+        dataset = sys.argv[3]
+
+    # Override region of interest
+    regions = icesat2.toregion(filename, tolerance)
 
     # Query CMR for list of resources
     for i in range(len(regions)):
         region = regions[i]
         resources = icesat2.cmr(region, short_name=dataset)
-        print("\nRegion:", i)
+        print("\nRegion {}: {} points, {} files".format(i, len(region), len(resources)))
         for resource in resources:
             print(resource)
